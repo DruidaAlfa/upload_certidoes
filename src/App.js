52e9c6c13fec70115e1aa223f37ad7ae9f057e79
DruidaAlfa <Rafaeligor.gui@gmail.com>
2024-06-19 11:09:59 -0300

    versão 1.2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Logo from "./img/logo.png"
+import Perfil from "./img/p2.png"
+
 
 function App() {
+
+  const [file, setFile] = useState()
+
+  function handleChange(event) {
+    setFile(event.target.files[0])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <nav className="navbar">
+      <div className="logo">
+        <img src={Logo} alt="Certidões Católicas" />
+      </div>
+      <div className="user-actions">
+        <img src={Perfil} alt="Acesso Restrito"  />
+      </div>
+    </nav>
+      <div className="row justify-content-center">
+        <div className="col-md-8">
+          <div className="card mt-4">
+            <div className="card-header">
+              <h3>Envio de Certidão</h3>
+            </div>
+
+            <div className="card-body">
+
+              <form>
+                <input type="file" onChange={handleChange}/>
+                  <div class="container">
+                    <div class="wrapper">
+                      <a href="#"><span>Enviar Certidão</span></a>
+                    </div>
+                  </div>
+              </form>
+              
+            </div>
+
+            <div className="card-header">
+              <a href="#"><span>Voltar</span></a>
+            </div>
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
